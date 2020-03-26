57790947c6d70c1683abc1a865046f9a9a034b40
zuzu-ai <sucelyalvarez@hotmail.com>
2020-03-26 11:13:48 -0600

    test branch changes

--- Data Base/bd_ins.sql
+++ Data Base/bd_ins.sql
@@ -2,8 +2,26 @@ create database bd_ins;
 use bd_ins;
 create table alumnos
 (
-Id int primary key auto_increment,
+IdAlumno varchar(5) primary key auto_increment,
 NombreAlumno varchar(60) not null,
-Grupo varchar(60) not null
+TelefonoAlumno varchar(8) not null,
+CorreoAlumno varchar(60) not null,
+DireccionAlumno varchar(60) not null,
+EstadoAlumno varchar(1) not  null,
+GrupoAlumno varchar(2) not null
 )engine=innodb;
+
+create table maestros
+(
+IdMaestro varchar(5) primary key auto_increment,
+NombreMaestro varchar(60) not null,
+TelefonoMaestro varchar(8) not null,
+CorreoMaestro varchar(60) not null,
+DireccionMaestro varchar(60) not null,
+EstadoMaestro varchar(1) not  null,
+GrupoMaestro varchar(2) not null,
+SueldoMaestro float(10) not null
+)engine=innodb;
+
 select * from alumnos;
+select * from maestros;
